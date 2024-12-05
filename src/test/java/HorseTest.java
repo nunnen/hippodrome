8f7b26f54e99a1b7f9ab910284d9c3836d0be3c7
Niyaz <niazsajdullin@gmail.com>
2024-12-05 20:08:08 +0500

    add blank tests

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+class HorseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testConstructorThrowsException() {
+    }
+
+    @Test
+    void testConstructorAtExceptionWritesMessage() {
+    }
+
+    @Test
+    void testConstructorThrowsExceptionIfBlank() {
+    }
+
+    @Test
+    void testConstructorAtExceptionIfBlankWritesMessage() {
+    }
+
+    @Test
+    void testConstructorThrowsExceptionIfSpeedNegative() {
+    }
+
+    @Test
+    void testConstructorAtExceptionIfSpeedNegativeWritesMessage() {
+    }
+
+    @Test
+    void testConstructorThrowsExceptionIfDistanceNegative() {
+    }
+
+    @Test
+    void testConstructorAtExceptionIfDistanceNegativeWritesMessage() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getSpeed() {
+    }
+
+    @Test
+    void getDistance() {
+    }
+
+    @Test
+    void move() {
+    }
+}
